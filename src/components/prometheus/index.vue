a3553a0c825c4de1079b4fa8a77910d6450eaeee
“zhiqimei” <18979109667@163.com>
2024-03-29 11:16:38 +0800

    ui first commit

---
+++ src/components/prometheus/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div >
+        <iframe src="http://localhost:9090/" id="map" scrolling="no" frameborder="0" style="position:absolute;top:4px;left: 210px;right:0px;bottom:10px;"></iframe>
+    </div>
+</template>
+ 
+ 
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        mounted(){
+            /**
+            * iframe-宽高自适应显示   
+            */
+            function changeMapIframe(){
+                const map = document.getElementById('map');
+                const deviceWidth = document.body.clientWidth;
+                const deviceHeight = document.body.clientHeight;
+                map.style.width = (Number(deviceWidth)-240) + 'px'; //数字是页面布局宽度差值
+                map.style.height = (Number(deviceHeight)+764) + 'px'; //数字是页面布局高度差
+            }
+
+            changeMapIframe()
+
+            window.onresize = function(){
+                changeMapIframe()
+            }
+        }
+    }
+</script>
